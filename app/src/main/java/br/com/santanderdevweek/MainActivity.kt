e194f7001f373aed31ba74aa5c62cd23f26e0bc8
Gabriel Freire <gfreire.santos95@gmail.com>
2021-06-01 23:58:41 -0300

    Criação de layout da home, adição de novos icones e alteração das cores do app

--- app/src/main/java/br/com/santanderdevweek/MainActivity.kt
+++ app/src/main/java/br/com/santanderdevweek/MainActivity.kt
@@ -2,10 +2,32 @@ package br.com.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.main_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_notificacoes -> {
+                Toast.makeText(this, "Menu foi clicado", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
