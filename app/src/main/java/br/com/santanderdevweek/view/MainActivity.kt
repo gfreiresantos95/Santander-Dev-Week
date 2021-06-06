28a2417caa2348ff4babf0d2eaf18731fe5ef4f8
Gabriel Freire <gfreire.santos95@gmail.com>
2021-06-05 22:46:25 -0300

    Ajustando layout de funções secundárias e card de cartão colapsado

--- app/src/main/java/br/com/santanderdevweek/view/MainActivity.kt
+++ app/src/main/java/br/com/santanderdevweek/view/MainActivity.kt
@@ -1,15 +1,23 @@
 package br.com.santanderdevweek.view
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import br.com.santanderdevweek.R
 import br.com.santanderdevweek.viewmodel.MainViewModel
+import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         mMainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         getData()
+        setListeners()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -61,6 +70,38 @@ class MainActivity : AppCompatActivity() {
 
             findViewById<TextView>(R.id.tv_final_card).text =
                 getString(R.string.cartao_final, conta.cartao.numero)
+
+            findViewById<TextView>(R.id.tv_card_due_date).text =
+                getString(R.string.validade_cartao, conta.cartao.dataValidade)
+
+            findViewById<TextView>(R.id.tv_card_shipping_date).text =
+                getString(R.string.cliente_desde, conta.cartao.dataExpedicao)
         })
+    }
+
+    private fun setListeners() {
+        findViewById<ConstraintLayout>(R.id.cl_final_card_fixed_container).setOnClickListener {
+            if (findViewById<ConstraintLayout>(R.id.cl_final_card_expandable_container).visibility == View.VISIBLE) {
+                TransitionManager.beginDelayedTransition(
+                    findViewById<ConstraintLayout>(R.id.cl_final_card_expandable_container),
+                    AutoTransition()
+                )
+
+                findViewById<ConstraintLayout>(R.id.cl_final_card_expandable_container).visibility =
+                    View.GONE
+
+                findViewById<ImageView>(R.id.iv_expand_final_card_icon).setImageResource(R.drawable.ic_expand_more)
+            } else {
+                TransitionManager.beginDelayedTransition(
+                    findViewById<ConstraintLayout>(R.id.cl_final_card_expandable_container),
+                    AutoTransition()
+                )
+
+                findViewById<ConstraintLayout>(R.id.cl_final_card_expandable_container).visibility =
+                    View.VISIBLE
+
+                findViewById<ImageView>(R.id.iv_expand_final_card_icon).setImageResource(R.drawable.ic_expand_less)
+            }
+        }
     }
 }
